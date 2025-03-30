6846efd4a5facc650f0967bf75404b8090487497
Aaron <aaron.scott@informed.com>
2025-03-30 14:09:43 +0100

    Initial commit - frontend scaffold

---
+++ frontend/vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <h1>Guess Word 🔠</h1>
+        <p>Let’s build something awesome 🧠</p>
+      </header>
+      <main className ="game-area">
+        <p>This is where the game will go.</p>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
